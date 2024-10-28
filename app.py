6d7f6e71255427067dc3f63c7d7364a3fe97f4bf
Gianni Perez <gianniperez@gmail.com>
2024-10-28 15:16:45 -0400

    incremental changes

--- app.py
+++ app.py
@@ -107,6 +107,6 @@ if st.sidebar.button("Generate L-System"):
 
 # Footer in Sidebar with smaller GitHub link
 st.sidebar.markdown("""
-<br>
+<br>#
 See my original Python code on GitHub: [LINK](https://github.com/ambron60/l-system-drawing)
 """, unsafe_allow_html=True)
